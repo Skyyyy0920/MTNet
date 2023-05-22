74f0bb230cf691698a8a4358776bdf60e45e506c
Tianhao Alexander Huang <huangtianhao0920@126.com>
2023-05-22 23:27:16 +0800

    Change the save epoch.
    Signed-off-by: Tianhao Alexander Huang <huangtianhao0920@126.com>

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
         logging.info(f"Current epoch's mean loss: {np.mean(loss_list)}\t\tlr: {optimizer.param_groups[0]['lr']}")
 
         # Save model
-        if (epoch + 1) % 30 == 0:
+        if (epoch + 1) % 60 == 0:
             checkpoint = {
                 'model_state': TreeLSTM_model.state_dict(),
                 'optimizer_state': optimizer.state_dict(),
@@ -273,7 +273,7 @@ if __name__ == '__main__':
             y_label_cat_numpy, y_pred_cat_numpy = get_pred_label(y_label_cat_list, y_pred_cat_list)
             y_label_coo_numpy, y_pred_coo_numpy = get_pred_label(y_label_coo_list, y_pred_coo_list)
 
-            if (epoch + 1) % 30 == 0:
+            if (epoch + 1) % 60 == 0:
                 pickle.dump(y_pred_POI_numpy, open(os.path.join(save_dir, f"recommendation_list_{epoch + 1}"), 'wb'))
                 pickle.dump(y_label_POI_numpy, open(os.path.join(save_dir, f"ground_truth_{epoch + 1}"), 'wb'))
 
