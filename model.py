1f563e848a4d3fbcbf2ab988dc977ef9ed4c719e
Tianhao Alexander Huang <huangtianhao0920@126.com>
2023-07-30 20:30:52 +0800

    V1.0
    Signed-off-by: Tianhao Alexander Huang <huangtianhao0920@126.com>

--- model.py
+++ model.py
@@ -61,28 +61,29 @@ class TreeLSTM(nn.Module):
         self.POI_embedding = nn.Embedding(num_embeddings=num_POIs, embedding_dim=POI_embed_dim)
         self.cat_embedding = nn.Embedding(num_embeddings=num_cats, embedding_dim=cat_embed_dim)
         self.coo_embedding = nn.Embedding(num_embeddings=num_coos, embedding_dim=coo_embed_dim)
-        self.user_embedding_o = nn.Embedding(num_embeddings=num_users, embedding_dim=user_embed_dim)
-        self.POI_embedding_o = nn.Embedding(num_embeddings=num_POIs, embedding_dim=POI_embed_dim)
-        self.cat_embedding_o = nn.Embedding(num_embeddings=num_cats, embedding_dim=cat_embed_dim)
-        self.coo_embedding_o = nn.Embedding(num_embeddings=num_coos, embedding_dim=coo_embed_dim)
+        # self.user_embedding_o = nn.Embedding(num_embeddings=num_users, embedding_dim=user_embed_dim)
+        # self.POI_embedding_o = nn.Embedding(num_embeddings=num_POIs, embedding_dim=POI_embed_dim)
+        # self.cat_embedding_o = nn.Embedding(num_embeddings=num_cats, embedding_dim=cat_embed_dim)
+        # self.coo_embedding_o = nn.Embedding(num_embeddings=num_coos, embedding_dim=coo_embed_dim)
         # positional encoding
         self.time_pos_encoder = nn.Embedding(num_embeddings=96, embedding_dim=self.embedding_dim)
-        self.time_pos_encoder_o = nn.Embedding(num_embeddings=96, embedding_dim=self.embedding_dim)
+        # self.time_pos_encoder_o = nn.Embedding(num_embeddings=96, embedding_dim=self.embedding_dim)
         # dropout
         self.embed_dropout = nn.Dropout(embed_dropout)
         self.model_dropout = nn.Dropout(model_dropout)
         # cell
         self.cell = Cell(self.embedding_dim, h_size, nary)
-        self.cell_o = Cell(self.embedding_dim, h_size, nary)
+        # self.cell_o = Cell(self.embedding_dim, h_size, nary)
         # decoder
         self.decoder_POI = nn.Linear(h_size, num_POIs)
         self.decoder_cat = nn.Linear(h_size, num_cats)
         self.decoder_coo = nn.Linear(h_size, num_coos)
-        self.decoder_POI_o = nn.Linear(h_size, num_POIs)
-        self.decoder_cat_o = nn.Linear(h_size, num_cats)
-        self.decoder_coo_o = nn.Linear(h_size, num_coos)
+        # self.decoder_POI_o = nn.Linear(h_size, num_POIs)
+        # self.decoder_cat_o = nn.Linear(h_size, num_cats)
+        # self.decoder_coo_o = nn.Linear(h_size, num_coos)
 
-    def forward(self, in_trees, out_trees):
+    # def forward(self, in_trees, out_trees):
+    def forward(self, in_trees):
         user_embedding = self.user_embedding(in_trees.features[:, 0].long() * in_trees.mask)
         POI_embedding = self.POI_embedding(in_trees.features[:, 1].long() * in_trees.mask)
         cat_embedding = self.cat_embedding(in_trees.features[:, 2].long() * in_trees.mask)
@@ -91,13 +92,13 @@ class TreeLSTM(nn.Module):
         concat_embedding = torch.cat((user_embedding, POI_embedding, cat_embedding, coo_embedding), dim=1)
         concat_embedding = concat_embedding + pe * 0.5
 
-        user_embedding_o = self.user_embedding_o(out_trees.features[:, 0].long() * out_trees.mask)
-        POI_embedding_o = self.POI_embedding_o(out_trees.features[:, 1].long() * out_trees.mask)
-        cat_embedding_o = self.cat_embedding_o(out_trees.features[:, 2].long() * out_trees.mask)
-        coo_embedding_o = self.coo_embedding_o(out_trees.features[:, 3].long() * out_trees.mask)
-        pe_o = self.time_pos_encoder_o(out_trees.time.long() * out_trees.mask)
-        concat_embedding_o = torch.cat((user_embedding_o, POI_embedding_o, cat_embedding_o, coo_embedding_o), dim=1)
-        concat_embedding_o = concat_embedding_o + pe_o * 0.5
+        # user_embedding_o = self.user_embedding_o(out_trees.features[:, 0].long() * out_trees.mask)
+        # POI_embedding_o = self.POI_embedding_o(out_trees.features[:, 1].long() * out_trees.mask)
+        # cat_embedding_o = self.cat_embedding_o(out_trees.features[:, 2].long() * out_trees.mask)
+        # coo_embedding_o = self.coo_embedding_o(out_trees.features[:, 3].long() * out_trees.mask)
+        # pe_o = self.time_pos_encoder_o(out_trees.time.long() * out_trees.mask)
+        # concat_embedding_o = torch.cat((user_embedding_o, POI_embedding_o, cat_embedding_o, coo_embedding_o), dim=1)
+        # concat_embedding_o = concat_embedding_o + pe_o * 0.5
 
         g = in_trees.graph.to(self.device)
         n = g.num_nodes()
@@ -119,28 +120,29 @@ class TreeLSTM(nn.Module):
         y_pred_cat = self.decoder_cat(h)
         y_pred_coo = self.decoder_coo(h)
 
-        g_o = out_trees.graph.to(self.device)
-        n = g_o.num_nodes()
-        g_o.ndata["iou"] = self.cell_o.W_iou(self.embed_dropout(concat_embedding_o)) \
-                           * out_trees.mask.float().unsqueeze(-1)
-        g_o.ndata["x"] = self.embed_dropout(concat_embedding_o) * out_trees.mask.float().unsqueeze(-1)
-        g_o.ndata["h"] = torch.zeros((n, self.h_size)).to(self.device)
-        g_o.ndata["c"] = torch.zeros((n, self.h_size)).to(self.device)
-        g_o.ndata["h_child"] = torch.zeros((n, self.nary, self.h_size)).to(self.device)
-        g_o.ndata["c_child"] = torch.zeros((n, self.nary, self.h_size)).to(self.device)
+        # g_o = out_trees.graph.to(self.device)
+        # n = g_o.num_nodes()
+        # g_o.ndata["iou"] = self.cell_o.W_iou(self.embed_dropout(concat_embedding_o)) \
+        #                    * out_trees.mask.float().unsqueeze(-1)
+        # g_o.ndata["x"] = self.embed_dropout(concat_embedding_o) * out_trees.mask.float().unsqueeze(-1)
+        # g_o.ndata["h"] = torch.zeros((n, self.h_size)).to(self.device)
+        # g_o.ndata["c"] = torch.zeros((n, self.h_size)).to(self.device)
+        # g_o.ndata["h_child"] = torch.zeros((n, self.nary, self.h_size)).to(self.device)
+        # g_o.ndata["c_child"] = torch.zeros((n, self.nary, self.h_size)).to(self.device)
+        #
+        # dgl.prop_nodes_topo(graph=g_o,
+        #                     message_func=self.cell_o.message_func,
+        #                     reduce_func=self.cell_o.reduce_func,
+        #                     apply_node_func=self.cell_o.apply_node_func)
+        #
+        # h_o = self.model_dropout(g_o.ndata.pop("h"))  # [batch_size, h_size]
+        #
+        # y_pred_POI_o = self.decoder_POI_o(h_o)
+        # y_pred_cat_o = self.decoder_cat_o(h_o)
+        # y_pred_coo_o = self.decoder_coo_o(h_o)
 
-        dgl.prop_nodes_topo(graph=g_o,
-                            message_func=self.cell_o.message_func,
-                            reduce_func=self.cell_o.reduce_func,
-                            apply_node_func=self.cell_o.apply_node_func)
-
-        h_o = self.model_dropout(g_o.ndata.pop("h"))  # [batch_size, h_size]
-
-        y_pred_POI_o = self.decoder_POI_o(h_o)
-        y_pred_cat_o = self.decoder_cat_o(h_o)
-        y_pred_coo_o = self.decoder_coo_o(h_o)
-
-        return y_pred_POI, y_pred_cat, y_pred_coo, y_pred_POI_o, y_pred_cat_o, y_pred_coo_o
+        # return y_pred_POI, y_pred_cat, y_pred_coo, y_pred_POI_o, y_pred_cat_o, y_pred_coo_o
+        return y_pred_POI, y_pred_cat, y_pred_coo
 
 
 class MultiTaskLoss(nn.Module):
